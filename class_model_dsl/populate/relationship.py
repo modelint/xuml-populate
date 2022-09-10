2932860dea5a1d97d39490f04ed44e7c70219f8a
Leon Starr <leon_starr@modelint.com>
2022-09-09 21:25:09 -0700

    Table dictionaries for binary association in rel subsystem populated

--- class_model_dsl/populate/relationship.py
+++ class_model_dsl/populate/relationship.py
@@ -4,6 +4,7 @@ relationship.py – Convert parsed relationship to a relation
 
 import logging
 
+
 class Relationship:
     """
     Create a relationship relation
@@ -17,7 +18,25 @@ class Relationship:
         self.subsys = subsys
         self.parse_data = parse_data
         self.rnum = parse_data['rnum']
+        self.t_side = parse_data['t_side']
+        self.p_side = parse_data['p_side']
 
         # Populate relationship
         self.domain.model.Insert('Relationship', [self.rnum, self.domain.name])
+        self.domain.model.Insert('Association', [self.rnum, self.domain.name])
+        self.domain.model.Insert('Binary Association', [self.rnum, self.domain.name])
+
+        self.domain.model.Insert('Perspective',
+                                 ['T', self.rnum, self.domain.name, self.t_side['cname'], self.t_side['phrase'],
+                                  True if 'c' in self.t_side['mult'] else False, self.t_side['mult'][0]]
+                                 )
+        self.domain.model.Insert('Asymmetric Perspective', ['T', self.rnum, self.domain.name])
+        self.domain.model.Insert('T Perspective', [self.rnum, self.domain.name])
+
+        self.domain.model.Insert('Perspective',
+                                 ['P', self.rnum, self.domain.name, self.p_side['cname'], self.p_side['phrase'],
+                                  True if 'c' in self.p_side['mult'] else False, self.p_side['mult'][0]]
+                                 )
+        self.domain.model.Insert('Asymmetric Perspective', ['P', self.rnum, self.domain.name])
+        self.domain.model.Insert('P Perspective', [self.rnum, self.domain.name])
         print()
