6f44460ad18720814d12350938d3ce926c3364e3
Leon Starr <leon_starr@modelint.com>
2022-09-10 13:53:20 -0700

    Association class rel populates form, rel but not class attrs subsys attr reference

--- class_model_dsl/populate/relationship.py
+++ class_model_dsl/populate/relationship.py
@@ -20,13 +20,21 @@ class Relationship:
         self.rnum = parse_data['rnum']
         self.t_side = parse_data['t_side']
         self.p_side = parse_data['p_side']
-        self.ref1 = parse_data['ref1']
+        self.ref1_source = parse_data['ref1']['source']
+        self.ref1_target = parse_data['ref1']['target']
         self.ref2 = parse_data.get('ref2')  # Supplied only for an associative binary
+        self.ref2_source = None if not self.ref2 else self.ref2['source']
+        self.ref2_target = None if not self.ref2 else self.ref2['target']
+        self.assoc_cname = parse_data.get('assoc_cname')
+        self.assoc_mult = parse_data.get('assoc_mult')
 
         # Populate relationship
         self.domain.model.Insert('Relationship', [self.rnum, self.domain.name])
         self.domain.model.Insert('Association', [self.rnum, self.domain.name])
         self.domain.model.Insert('Binary Association', [self.rnum, self.domain.name])
+        if self.assoc_cname:
+            self.domain.model.Insert('Association Class', [self.rnum, self.assoc_cname, self.domain.name])
+
 
         # Populate the T and P perspectives of an asymmetric binary association
         self.domain.model.Insert('Perspective',
@@ -44,13 +52,36 @@ class Relationship:
         self.domain.model.Insert('P Perspective', [self.rnum, self.domain.name])
 
         # Create reference
-        self.domain.model.Insert('Reference', ['R', self.source['class'], self.target['class'], self.rnum, self.domain.name])
-        referenced_perspective = 'T' if self.target['class'] == self.t_side['cname'] else 'P'
-        self.domain.model.Insert('Association Reference',
-                           ['R', self.source['class'], self.target['class'], self.rnum, self.domain.name,
-                            referenced_perspective])
-        self.domain.model.Insert('Simple Association Reference',
-                           [self.source['class'], self.target['class'], self.rnum, self.domain.name])
-        self.domain.model.Insert('Referring Class', [self.rnum, self.source['class'], self.domain.name])
-        self.domain.model.Insert('Formalizing Class Role', [self.rnum, self.source['class'], self.domain.name])
+        if not self.ref2:  # Simple binary association
+            self.domain.model.Insert('Reference', ['R', self.ref1_source['class'], self.ref1_source['class'], self.rnum, self.domain.name])
+            referenced_perspective = 'T' if self.ref1_target['class'] == self.t_side['cname'] else 'P'
+            self.domain.model.Insert('Association Reference',
+                               ['R', self.ref1_source['class'], self.ref1_target['class'], self.rnum, self.domain.name,
+                                referenced_perspective])
+            self.domain.model.Insert('Simple Association Reference',
+                               [self.ref1_source['class'], self.ref1_target['class'], self.rnum, self.domain.name])
+            self.domain.model.Insert('Referring Class', [self.rnum, self.ref1_source['class'], self.domain.name])
+            self.domain.model.Insert('Formalizing Class Role', [self.rnum, self.ref1_source['class'], self.domain.name])
+        else:  # Binary associative (with association class)
+            # T Reference
+            self.domain.model.Insert('Reference', ['T', self.ref1_source['class'], self.ref1_target['class'], self.rnum, self.domain.name])
+            self.domain.model.Insert('Formalizing Class Role', [self.rnum, self.assoc_cname, self.domain.name])
+
+            self.domain.model.Insert('Association Reference',
+                                     ['T', self.ref1_source['class'], self.ref1_target['class'], self.rnum, self.domain.name,
+                                      'T'])
+            self.domain.model.Insert('Association Class Reference',
+                                     ['T', self.ref1_source['class'], self.ref1_target['class'], self.rnum, self.domain.name])
+            self.domain.model.Insert('T Reference',
+                                     [self.ref1_source['class'], self.ref1_target['class'], self.rnum, self.domain.name])
+
+            # P Reference
+            self.domain.model.Insert('Reference', ['P', self.ref2_source['class'], self.ref2_target['class'], self.rnum, self.domain.name])
+            self.domain.model.Insert('Association Reference',
+                                     ['P', self.ref2_source['class'], self.ref2_target['class'], self.rnum, self.domain.name,
+                                      'P'])
+            self.domain.model.Insert('Association Class Reference',
+                                     ['P', self.ref2_source['class'], self.ref2_target['class'], self.rnum, self.domain.name])
+            self.domain.model.Insert('P Reference',
+                                     [self.ref2_source['class'], self.ref1_target['class'], self.rnum, self.domain.name])
         print()
