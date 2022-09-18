ca032150145a60079d81102e904dc798c2446884
Leon Starr <leon_starr@modelint.com>
2022-09-17 18:05:31 -0700

    Loaded ordinal relationship into DB

--- class_model_dsl/populate/ordinal.py
+++ class_model_dsl/populate/ordinal.py
@@ -14,3 +14,17 @@ class Ordinal:
         self.logger = logging.getLogger(__name__)
 
         self.relationship = relationship
+        self.ascend = relationship.parse_data['ascend']
+        self.oform = relationship.parse_data['oform']
+
+        # Populate
+        self.logger.info(f"Populating Ordinal [{self.relationship.rnum}]")
+        self.relationship.domain.model.Insert('Ordinal Relationship', [
+            self.relationship.rnum,
+            self.relationship.domain.name,
+            self.ascend['cname'],
+            self.oform['ranking attr'],
+            self.oform['id'],
+            self.ascend['highval'],
+            self.ascend['lowval'],
+        ])
