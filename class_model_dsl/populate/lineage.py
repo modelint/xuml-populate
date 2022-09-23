7df9254e42f3aca699e7b5bc15ed7d578131a8e9
Leon Starr <leon_starr@modelint.com>
2022-09-23 10:17:11 -0700

    Added lineage test pattern input

--- class_model_dsl/populate/lineage.py
+++ class_model_dsl/populate/lineage.py
@@ -3,7 +3,8 @@ lineage.py – Compute all lineage instances and populate them
 """
 
 import logging
-
+from class_model_dsl.database.sm_meta_db import SMmetaDB as smdb
+from sqlalchemy import select, join, and_
 
 class Lineage:
     """
@@ -16,3 +17,14 @@ class Lineage:
 
         self.domain = domain
         self.logger.info(f"Skipping lineage for now")
+        self.leaf = {}
+
+        leaf_classes = None
+        # Get all classes with no superclass facets and at least on subclass facet
+        subclass_t = smdb.MetaData.tables['Subclass']
+
+
+    def Trace(self, key, c, rel, lineage):
+        """
+        Add classes to a leaf lineage
+        """
