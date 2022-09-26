7c8d8c8a9acce2c1f322d4675b2cd6a391e7a13f
Leon Starr <leon_starr@modelint.com>
2022-09-25 18:13:39 -0700

    Partial success. DEFH case tests okay

--- class_model_dsl/populate/lineage.py
+++ class_model_dsl/populate/lineage.py
@@ -4,9 +4,60 @@ lineage.py – Compute all lineage instances and populate them
 
 import logging
 from class_model_dsl.database.sm_meta_db import SMmetaDB as smdb
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from typing import List, Set, Optional
 
+def findSubclasses(grel: str, domain: str) -> Set[str]:
+    """
+    Return the set of all subclasses in the specified generalization
+
+    :param grel:
+    :param domain:
+    :return:
+    """
+    subclass_t = smdb.MetaData.tables['Subclass']
+    p = subclass_t.c.Class  # Ideally we would project on zero attributes, but SQL requires at least one
+    r = and_(
+        (subclass_t.c.Rnum == grel),
+        (subclass_t.c.Domain == domain),
+    )
+    q = select(p).where(r)
+    rows = smdb.Connection.execute(q).fetchall()
+    return {r['Class'] for r in rows}
+
+
+def isSubclass(grel: str, cname: str, domain: str) -> bool:
+    subclass_t = smdb.MetaData.tables['Subclass']
+    p = subclass_t.c.Rnum  # Ideally we would project on zero attributes, but SQL requires at least one
+    r = and_(
+        (subclass_t.c.Class == cname),
+        (subclass_t.c.Rnum == grel),
+        (subclass_t.c.Domain == domain),
+    )
+    q = select(p).where(r)
+    # One returned row means it is a subclass, otherwise it must be a superclass
+    row = smdb.Connection.execute(q).fetchone()
+    return True if row else False
+
+
+def findSuperclass(grel: str, domain: str) -> str:
+    """
+    Traverse the specified relationship and return the name of the superclass
+
+    :param grel:  A generalization relationship rnum
+    :param domain:  A the name of the domain
+    :return:
+    """
+    superclass_t = smdb.MetaData.tables['Superclass']
+    p = superclass_t.c.Class  # We need the name of that superclass class
+    r = and_(
+        (superclass_t.c.Rnum == grel),
+        (superclass_t.c.Domain == domain),
+    )
+    q = select(p).where(r)
+    # One returned row means it is a subclass, otherwise it must be a superclass
+    row = smdb.Connection.execute(q).fetchone()
+    return row['Class']
 
 class Lineage:
     """
@@ -19,6 +70,8 @@ class Lineage:
 
         self.domain = domain
         self.walks = []
+        self.xrels = set()
+        self.xclasses = set()
 
         # Get all classes with at least one subclass facet and no superclass facets as: leaves
 
@@ -29,19 +82,77 @@ class Lineage:
         q = select(psub).except_(select(psuper))
         rows = smdb.Connection.execute(q).fetchall()
         self.leaf_classes = [r['Class'] for r in rows]
+        self.leaf_classes = ['D']  # TODO: Remove this line (here for testing only)
         for leaf in self.leaf_classes:
-            leafwalk = self.Step(walk=[], cvisit=leaf, xrels=set())
+            leafwalk = self.step(walk=[], cvisit=leaf, rvisit=None)
             self.walks.append(leafwalk)
         print()
 
-    def Step(self, walk: List, cvisit: str, xrels: Set[int], rvisit: Optional[int] = None) -> List:
+    def step(self, walk: List, cvisit: str, rvisit: Optional[str] = None) -> List:
+        """
+        Advance one step in the walk and return the result
+
+        :param walk:  The walk prior to the step
+        :param cvisit: The class currently being visited
+        :param rvisit: The relationship currently being traversed
+        :return: The updated walk
         """
 
-        :param walk:
-        :param cvisit:
-        :param rvisit:
-        :param xrels:
-        :return:
-        """
-        walk.append(cvisit)
+        walk.append(cvisit)  # Advance the walk by adding the visited class
+        self.xclasses.add(cvisit)  # Class has been visited
+        # Now we figure out where and if we can take another step
+
+        # Get all adjacent relationships, if any, on the civisit class that have not already been traversed
+        facet_t = smdb.MetaData.tables['Facet']  # Could be either super or subclasses, so we search Facets
+        p = facet_t.c.Rnum # We project on the rnums
+        r = and_(
+            (facet_t.c.Class == cvisit),
+            (facet_t.c.Domain == self.domain.name),
+        )  # Restrict on the cvisit class
+        q = select(p).where(r)  # Get all Facets that cvisit participates in
+        rows = smdb.Connection.execute(q).fetchall()
+        # Grab the result being careful to exclude prior traversals so we don't walk around in circles!
+        adj_rels = [r['Rnum'] for r in rows if r['Rnum'] not in self.xrels and r['Rnum'] != rvisit]
+        if cvisit == 'D':  # TODO: Remove this line here for testing only
+            adj_rels = ['R2', 'R4', 'R1']
+
+        # We have nowhere else to walk if cvisit does not participate in any new rels
+        if not adj_rels:
+            return walk
+
+        # We can try to take a step
+        for arel in adj_rels:
+            # Is cvisit a superclass or subclass in this arel?
+            # There are only two possibilities, so we arbitrarily check to see if it paticipates as a subclass
+
+            cvisit_is_subclass_in_arel = isSubclass(grel=arel, cname=cvisit, domain=self.domain.name)
+
+            if cvisit_is_subclass_in_arel:
+                superclass = findSuperclass(grel=arel, domain=self.domain.name)
+                subclasses = findSubclasses(grel=arel, domain=self.domain.name)
+                xclass_in_grel = (subclasses - set(cvisit)).intersection(self.xclasses)
+                if xclass_in_grel:
+                    print()
+                    return walk[:-1]  # Chop off this cvisit and terminate
+
+                # Since we don't need to branch out, we can now mark this arel as excluded
+                self.xrels.add(arel)
+                walk = self.step(walk=walk, cvisit=superclass, rvisit=arel)
+            else:  # cvisit is a superclass in arel
+                print()
+                # We are going to branch out to one or more subclasses
+                # (Any of our subclasses adjacent to some excluded relationship cannot be added)
+                # Get all the subclass class names
+                subclasses = findSubclasses(grel=arel, domain=self.domain.name)
+                for s in subclasses:
+                    print()
+                    branch = self.step(walk=[], cvisit=s, rvisit=arel)
+                    if branch:
+                        walk.append(branch)
+                    else:
+                        self.xclasses.remove(s)
+                        print()
+                    print()
+                self.xrels.add(arel)
+        print()
         return walk
