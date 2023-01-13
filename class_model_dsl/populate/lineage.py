f84caed18df5eba25142ca9ca7dd6456b03efffb
Leon Starr <leon_starr@modelint.com>
2023-01-13 14:19:14 -0800

    prepping lineage.step method for tclral

--- class_model_dsl/populate/lineage.py
+++ class_model_dsl/populate/lineage.py
@@ -27,6 +27,7 @@ class Lineage:
     xrels = set()
     xclasses = set()
     popclasses = set()
+    lineages = None
 
     @classmethod
     def Derive(cls, mmdb: 'Tk', domain: str):
@@ -48,33 +49,109 @@ class Lineage:
         r = Relation.subtract(db=mmdb, rname1='subs', rname2='supers')
         leaf_tuples = Relation.make_pyrel(relation=r, name='leaf tuples')
         leaf_classes = [t['Class'] for t in leaf_tuples.body]
+
+        # Now we walk (step) through each generalization to build trees of one or more lineages
+        for leaf in leaf_classes:
+            xrels = set()
+            xclasses = set()
+            leafwalk = cls.step(walk=[], cvisit=leaf, rvisit=None)
+            cls.walks.append(leafwalk)
+
+        # We then prune these trees to extract unique branches, each of which constitutes a distinct lineage
+        cls.lineages = set()
+        for walk in cls.walks:
+            pattern = walk.copy()
+            if not any(isinstance(n, list) for n in pattern):
+                pattern.sort()
+                cls.lineages.add(':'.join(pattern))
+            else:
+                while len(pattern) > 0 and any(isinstance(n, list) for n in pattern):
+                    extraction = extract(pattern)[0].copy()
+                    extraction.sort()
+                    cls.lineages.add(':'.join(extraction))
+
+        # Finally, we load each lineage into the db
+        cls.populate()
+
+
+    @classmethod
+    def step(cls, walk: List, cvisit: str, rvisit: Optional[str] = None) -> List:
+        """
+        Advance one step in the walk and return the result
+
+        :param walk:  The walk prior to the step
+        :param cvisit: The class currently being visited
+        :param rvisit: The relationship currently being traversed
+        :return: The updated walk
+        """
         pass
 
-#         # Now we walk (step) through each generalization to build trees of one or more lineages
-#         for leaf in self.leaf_classes:
-#             self.xrels = set()
-#             self.xclasses = set()
-#             leafwalk = self.step(walk=[], cvisit=leaf, rvisit=None)
-#             self.walks.append(leafwalk)
-#
-#         # We then prune these trees to extract unique branches, each of which constitutes a distinct lineage
-#         self.lineages = set()
-#         for walk in self.walks:
-#             pattern = walk.copy()
-#             if not any(isinstance(n, list) for n in pattern):
-#                 pattern.sort()
-#                 self.lineages.add(':'.join(pattern))
-#             else:
-#                 while len(pattern) > 0 and any(isinstance(n, list) for n in pattern):
-#                     extraction = extract(pattern)[0].copy()
-#                     extraction.sort()
-#                     self.lineages.add(':'.join(extraction))
-#
-#         # Finally, we load each lineage into the db
-#         self.populate()
+        walk.append(cvisit)  # Advance the walk by adding the visited class
+        cls.xclasses.add(cvisit)  # Class has been visited
+        # Now we figure out where and if we can take another step
 
+        # Get all adjacent relationships, if any, on the civisit class that have not already been traversed
+        facet_t = smdb.MetaData.tables['Facet']  # Could be either superclass_name or subclasses, so we search Facets
+        p = facet_t.c.Rnum  # We project on the rnums
+        r = and_(
+            (facet_t.c.Class == cvisit),
+            (facet_t.c.Domain == self.domain.name),
+        )  # Restrict on the cvisit class
+        q = select(p).where(r)  # Get all Facets that cvisit participates in
+        rows = smdb.Connection.execute(q).fetchall()
+        # Grab the result being careful to exclude prior traversals so we don't walk around in circles!
+        adj_rels = [r['Rnum'] for r in rows if r['Rnum'] not in self.xrels and r['Rnum'] != rvisit]
 
-#     @classmethod
+        # # We have nowhere else to walk if cvisit does not participate in any new rels
+        # if not adj_rels:
+        #     return walk
+        #
+        # # Create a set of all hops going up to a superclass
+        # uphops = {h for h in adj_rels if isSubclass(grel=h, cname=cvisit, domain=self.domain.name)}
+        #
+        # # We can try to take a step
+        # for arel in adj_rels:
+        #     # Is cvisit a superclass or subclass in this arel?
+        #     # There are only two possibilities, so we arbitrarily check to see if it paticipates as a subclass
+        #     if arel in uphops:  # Hopping up to a superclass
+        #         superclass = findSuperclass(grel=arel, domain=self.domain.name)
+        #
+        #         # Exclude all subclasses of unvisited uphop rels other than cvisit
+        #         other_uphops = uphops - self.xrels - {arel}
+        #         for o in other_uphops:
+        #             subclasses = findSubclasses(grel=o, domain=self.domain.name)
+        #             exclude_subclasses = {c for c in subclasses if c != cvisit}
+        #             self.xclasses = self.xclasses.union(exclude_subclasses)
+        #
+        #         # Since we don't need to branch out, we can now mark this arel as excluded
+        #         self.xrels.add(arel)
+        #         walk = self.step(walk=walk, cvisit=superclass, rvisit=arel)
+        #     else:  # hopping down to a subclass
+        #         print()
+        #         # We are going to branch out to one or more subclasses
+        #         # (Any of our subclasses adjacent to some excluded relationship cannot be added)
+        #         # Get all the subclass class names
+        #         subclasses = findSubclasses(grel=arel, domain=self.domain.name)
+        #         visit_subs = subclasses.difference(self.xclasses)
+        #         for s in visit_subs:
+        #             print()
+        #             # Start a new branch if there is more than one subclass to visit
+        #             fork = True if len(visit_subs) > 1 else False
+        #             if fork:
+        #                 self.xrels = set()  # New branch, no excluded rels
+        #                 branch = self.step(walk=[], cvisit=s, rvisit=arel)
+        #                 if branch:
+        #                     walk.append(branch)
+        #                 else:
+        #                     self.xclasses.remove(s)
+        #                     print()
+        #             else:
+        #                 walk = self.step(walk=walk, cvisit=s, rvisit=arel)
+        #         self.xrels.add(arel)
+        # print()
+        return walk
+
+    #     @classmethod
 #     def findSubclasses(cls, grel: str, domain: str) -> Set[str]:
 #         """
 #         Return the set of all subclasses in the specified generalization
@@ -178,12 +255,14 @@ class Lineage:
 #         # Finally, we load each lineage into the db
 #         self.populate()
 #
-#     def populate(self):
-#         """
-#         Trace through walks to populate all Lineages
-#
-#         :return:
-#         """
+    @classmethod
+    def populate(cls):
+        """
+        Trace through walks to populate all Lineages
+
+        :return:
+        """
+        pass
 #         population = OrderedDict({'Element': [], 'Spanning Element': [], 'Lineage': [], 'Class in Lineage': []})
 #         for lin in self.lineages:
 #             self.domain.lnums += 1
@@ -199,7 +278,8 @@ class Lineage:
 #             t = smdb.Relvars[relvar_name]
 #             smdb.Connection.execute(t.insert(), relation)
 #
-#     def step(self, walk: List, cvisit: str, rvisit: Optional[str] = None) -> List:
+        # @classmethod
+        # def step(cls, walk: List, cvisit: str, rvisit: Optional[str] = None) -> List:
 #         """
 #         Advance one step in the walk and return the result
 #
