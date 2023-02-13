d7d63f2982ea359a02cb17e863f6dcc4c87ed768
Leon Starr <leon_starr@modelint.com>
2023-02-13 07:19:22 -0800

    better formulation of comments nl that works

--- class_model_dsl/populate/activity.py
+++ class_model_dsl/populate/activity.py
@@ -44,7 +44,7 @@ class Activity:
 
     @classmethod
     def parse(cls, actions, debug=False):
-        test_text = "Cabin at destination ->\n    /R53/Transfer\n"
+        test_text = "Cabin at destination -> // wrapping\n    /R53/Transfer //another comment \n"
         print(test_text)
         result = ScrallParser.parse(scrall_text=test_text, debug=True)
         pass
