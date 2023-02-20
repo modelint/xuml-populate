595c2fbfdc5e33845169ada06a146993b2ee107c
Leon Starr <leon_starr@modelint.com>
2023-02-20 14:29:51 -0800

    inst.scrall test parses with both calls and comma sep selections

--- class_model_dsl/test/scrall/inst/scrall_visitor.py
+++ class_model_dsl/test/scrall/inst/scrall_visitor.py
@@ -6,6 +6,8 @@ Supplied_Parameter_a = namedtuple('Supplied_Parameter_a', 'pname sval')
 """Parameter name and flow name pair for a set of supplied parameters"""
 Call_a = namedtuple('Call_a', 'op_name supplied_params dest')
 Attr_Access_a = namedtuple('Attr_Access_a', 'cname attr')
+Attr_Comparison_a = namedtuple('Attr_Comparison_a', 'attr op scalar')
+Selection_a = namedtuple('Selection_a', 'cname card criteria')
 
 class ScrallVisitor(PTNodeVisitor):
 
@@ -13,6 +15,30 @@ class ScrallVisitor(PTNodeVisitor):
         return [c for c in children if c]
 
     def visit_statement(self, node, children):
+        return children
+
+    def visit_instance_set(self, node, children):
+        return {'iset' : children}
+
+    def visit_selection(self, node, children):
+        return Selection_a(cname=children[0], card=children[1][0], criteria=None if len(children[1]) != 2 else children[1][1])
+
+    def visit_select_phrase(self, node, children):
+        char1 = children[0][0]
+        explicit_card = char1 if char1 in ('1', '*') else None
+        card = explicit_card if explicit_card else '*'
+        if len(children) == 1 and explicit_card:
+            return [card]
+        else:
+            return [card, children[-1]]
+
+    def visit_criteria(self, node, children):
+        return children
+
+    def visit_attr_comparison(self, node, children):
+        return Attr_Comparison_a(attr=children[0], op=children[1][0], scalar=children[1][1])
+
+    def visit_comparison(self, node, children):
         return children
 
     def visit_call(self, node, children):
