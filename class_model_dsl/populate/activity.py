0ad06542eff5e408564047e1dd7a85a5676bb8b0
Leon Starr <leon_starr@modelint.com>
2023-02-20 14:54:29 -0800

    Setup new path_inst dir to test combined inc grammar tests

--- class_model_dsl/populate/activity.py
+++ class_model_dsl/populate/activity.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from PyRAL.relvar import Relvar
 from class_model_dsl.populate.element import Element
 # from class_model_dsl.parse.scrall_parser import ScrallParser
-from class_model_dsl.test.scrall.inst.scrall_parser import ScrallParser
+from class_model_dsl.test.scrall.path_inst.scrall_parser import ScrallParser
 from typing import TYPE_CHECKING
 from class_model_dsl.populate.pop_types import Activity_i, Asynchronous_Activity_i,\
     State_Activity_i, Signature_i, Parameter_i
@@ -47,8 +47,8 @@ class Activity:
     @classmethod
     def parse(cls, actions, debug=False):
         # Read the test file
-        test_file_dir = Path(__file__).parent.parent / "test" / "scrall" / "inst"
-        test_file_path = test_file_dir / "inst.scrall"
+        test_file_dir = Path(__file__).parent.parent / "test" / "scrall" / "path_inst"
+        test_file_path = test_file_dir / "path-inst.scrall"
         test_text = open(test_file_path, 'r').read() + "\n"
 
         # test_text = "TRAN.Goto floor(\n      floor : Height,\n   direction\n  )\n"
