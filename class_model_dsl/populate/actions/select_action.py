a37e79e5a5afe5400d79bfdc815c5401ca016026
Leon Starr <leon_starr@modelint.com>
2023-07-18 09:17:36 -0700

    Added recursive selection criteria walk, deleted old scrall peg

--- class_model_dsl/populate/actions/select_action.py
+++ class_model_dsl/populate/actions/select_action.py
@@ -25,14 +25,57 @@ class SelectAction:
     input_flow = None
 
     @classmethod
-    def process_criteria(cls, criteria):
-        for c in criteria:
-            match type(c).__name__:
+    def process_flow(cls, name:str, input_param:bool):
+        print(f"Populating [{name}] as flow")
+
+    @classmethod
+    def process_attr(cls, name):
+        print(f"Populating [{name}] as attribute")
+        cls.attr_set = True
+
+    @classmethod
+    def walk_criteria(cls, operator, operands, attr: bool = False):
+        attr_set = attr
+        # left_op = operands[0]
+        # right_op = None if len(operands) < 2 else operands[1]
+        for o in operands:
+            match type(o).__name__:
+                case 'IN_a':
+                    cls.process_flow(o, True)
                 case 'N_a':
-                    # We have an attribute
+                    if not attr_set:
+                        cls.process_attr(o.name)
+                        attr_set = True
+                    else:
+                        cls.process_flow(o.name, False)
+                case 'BOOL_a':
+                    cls.walk_criteria(o.op, o.operands, attr_set)
+                case 'MATH_a':
                     print()
-                case _:
-                    _logger.error("No match case for criteria in select populate")
+                case 'UNARY_a':
+                    print()
+
+    @classmethod
+    def process_criteria(cls, criteria):
+        """
+        Break down criteria into a set of attribute comparisons and validate the components of a Select Action that
+        must be populated into the metamodel.
+
+        These components are:
+        * Restriction Criterian (Comparison or Ranking)
+        * Scalar Flow inputs to any Comparison Criterion
+
+        These components
+        Sift through criteria to ensure that each terminal is either an attribute, input flow, or value.
+
+
+        :param criteria:
+        :return:
+        """
+        # Criteria is a scalar expression
+        cls.walk_criteria(criteria.op, criteria.operands)
+        pass
+
 
     @classmethod
     def populate(cls, input_flow:str, select_agroup):
