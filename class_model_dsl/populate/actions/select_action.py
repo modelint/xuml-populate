f6c160c09dc69823350b42f4f393e54e4f63687f
Leon Starr <leon_starr@modelint.com>
2023-07-20 19:16:35 -0700

    Specified population of Select Action subclass instances

--- class_model_dsl/populate/actions/select_action.py
+++ class_model_dsl/populate/actions/select_action.py
@@ -6,7 +6,9 @@ import logging
 from typing import TYPE_CHECKING, Set, Dict, List, Optional
 from class_model_dsl.exceptions.action_exceptions import ComparingNonAttributeInSelection, NoInputInstanceFlow
 from class_model_dsl.populate.actions.action import Action
-from class_model_dsl.populate.pop_types import Select_Action_i
+from class_model_dsl.populate.flow import Flow
+from class_model_dsl.populate.pop_types import Select_Action_i, Single_Select_i, Identifer_Select_i,\
+    Zero_One_Cardinality_Select_i, Many_Select_i
 from pyral.relvar import Relvar
 from pyral.relation import Relation
 from pyral.transaction import Transaction
@@ -26,9 +28,9 @@ class SelectAction:
 
     input_instance_flow = None  # We are selecting instances from this instance flow
     cname = None  # We are performing a selection on this class
+    anum = None
     expression = None
     comparison_criteria = []
-    select_on_id = False
     restriction_text = ""
     cardinality = None
     action_id = None
@@ -36,7 +38,7 @@ class SelectAction:
     mmdb = None  # The database
 
     @classmethod
-    def identifier_selection(cls) -> bool:
+    def identifier_selection(cls) -> int:
         """
         | Determine whether we are selecting based on an identifier match.
         An identifier match supplies one value per identifier attribute for some identifier defined on
@@ -55,6 +57,7 @@ class SelectAction:
          | Accessible Shaft Level( Shaft == y )
          |
          | you may select multiple instances since a Shaft intersects multiple Floors
+         :returns: An identifier number 1,2, ... or 0 if none found
         """
         idcheck = {c['attr'] for c in cls.comparison_criteria if c['op'] == '=='}
         R = f"Class:<{cls.cname}>, Domain:<{cls.domain}>"
@@ -77,9 +80,9 @@ class SelectAction:
                 # The set of identifier attributes for the current id number
                 # is present in the set of attribute equivalence matches
                 # So we are selecting on an identifier and at most one instance can flow out of the selection
-                return True
+                return i
             i += 1  # Increment to the next I num (I1, I2, etc)
-        return False
+        return 0
 
     @classmethod
     def process_bool_value(cls, attr: str, setting: bool):
@@ -163,27 +166,53 @@ class SelectAction:
         """
         Break down criteria into a set of attribute comparisons and validate the components of a Select Action that
         must be populated into the metamodel.
-
-        These components are:
+         | These components are:
         * Restriction Criterian (Comparison or Ranking)
         * Scalar Flow inputs to any Comparison Criterion
 
-        These components
         Sift through criteria to ensure that each terminal is either an attribute, input flow, or value.
-
-
         :param criteria:
-        :return:
         """
-        # Criteria is a scalar expression
-        # Consider case where there is a single boolean value critieria
+        # Consider case where there is a single boolean value critieria such as:
+        #   shaft aslevs( Stop requested )
+        # The implication is that we are selecting on: Stop requested == true
+        # So elaborate the parse elminating our shorhand
         if type(criteria).__name__ == 'N_a':
+            # Name only (no explicit operator or operand)
             criteria = BOOL_a(op='==', operands=[criteria, N_a(name='true')])
-        op = criteria.op
-        operands = criteria.operands
-        cls.expression = cls.walk_criteria(op, operands)
-        cls.select_on_id = cls.identifier_selection()
-        pass
+        # Walk the parse tree and save all attributes, ops, values, and input scalar flows
+        cls.expression = cls.walk_criteria(criteria.op, criteria.operands)
+        # Determine if this should be an Identifier Select subclass that yields at most one instance
+        selection_idnum = cls.identifier_selection()
+        if selection_idnum or cls.cardinality == 'ONE':
+            # Populate a single instance flow for the selection output
+            output_flow_id = Flow.populate_instance_flow(cls.mmdb, cname=cls.cname, activity=cls.anum, domain=cls.domain,
+                                                         label=None, single=True)
+            # Populate the Single Select subclass
+            Relvar.insert(relvar='Single_Select', tuples=[
+                Single_Select_i(Action=cls.action_id, Activity=cls.anum, Domain=cls.domain, Output_flow=output_flow_id)
+            ])
+            if selection_idnum:
+                # Populate an Identifier Select subclass
+                Relvar.insert(relvar='Identifier_Select', tuples=[
+                    Identifer_Select_i(Action=cls.action_id, Activity=cls.anum, Domain=cls.domain,
+                                       Identifier=selection_idnum, Class=cls.cname)
+                ])
+            else:
+                # Populate an Identifier Select subclass
+                # Note that if both ONE cardinality specified and identifier select, identifier select takes precedence
+                Relvar.insert(relvar='Zero_One_Cardinality_Select', tuples=[
+                    Zero_One_Cardinality_Select_i(Action=cls.action_id, Activity=cls.anum, Domain=cls.domain)
+                ])
+        else:
+            # Many select with Multiple Instance Flow output
+            output_flow_id = Flow.populate_instance_flow(cls.mmdb, cname=cls.cname, activity=cls.anum,
+                                                         domain=cls.domain, label=None, single=False)
+            # Populate the Many Select subclass
+            Relvar.insert(relvar='Many_Select', tuples=[
+                Many_Select_i(Action=cls.action_id, Activity=cls.anum, Domain=cls.domain, Output_flow=output_flow_id)
+            ])
+
 
     @classmethod
     def populate(cls, mmdb: 'Tk', input_instance_flow: str, anum: str, select_agroup, domain: str):
@@ -196,23 +225,35 @@ class SelectAction:
         :param domain:
         :param select_agroup:  The parsed Scrall select action group
         """
+        # Save attribute values that we will need when creating the various select subsystem
+        # classes
         cls.mmdb = mmdb
         cls.domain = domain
-        cls.input_instance_flow = input_instance_flow
-        cls.action_id = Action.populate(mmdb, anum, domain)  # Transaction open
-        cls.cardinality = '1' if select_agroup.card == '1' else 'M'
-        # Determine this flow's Class Type
+        cls.anum = anum
+        # Cardinality is specified as 1 or M on the selection
+        # If 1, user wants at most one arbitrary instance, even if many are selected
+        # If M, get them all.
+        cls.cardinality = 'ONE' if select_agroup.card == '1' else 'ALL'
+
+        # Ensure that the input flow is, in fact, an instance flow and determine its Class Type
         R = f"ID:<{input_instance_flow}>, Activity:<{anum}>, Domain:<{cls.domain}>"
         result = Relation.restrict3(cls.mmdb, relation='Instance_Flow', restriction=R)
         if not result.body:
             raise NoInputInstanceFlow(f"select action input flow [{input_instance_flow}:{anum}:{domain}]"
                                       f" is not an instance flow")
+        cls.input_instance_flow = input_instance_flow
         cls.cname = result.body[0]['Class']
+
+        # Populate the Action superclass instance and obtain its action_id
+        cls.action_id = Action.populate(mmdb, anum, domain)  # Transaction open
         Relvar.insert(relvar='Select_Action', tuples=[
             Select_Action_i(Action=cls.action_id, Activity=anum, Class=cls.cname, Domain=domain,
                             Input_flow=cls.input_instance_flow, Selection_cardinality=cls.cardinality)
         ])
-
         cls.select_agroup = select_agroup
+        # Walk through the critieria parse tree storing any attributes or input flows
+        # Also check to see if we are selecting on an identifier
         cls.process_criteria(criteria=select_agroup.criteria)
+
+        # We now have a transaction with all select-action instances, enter into the metamodel db
         Transaction.execute()  # Select Action
