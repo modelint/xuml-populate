4d693b2069fb43529d41bd966799ff24e5ad5eb6
Leon Starr <leon_starr@modelint.com>
2025-04-17 16:15:24 -0700

    Updated for new cond wave metamodel

--- src/xuml_populate/populate/mmclass_nt.py
+++ src/xuml_populate/populate/mmclass_nt.py
@@ -68,6 +68,7 @@ Traverse_Action_i = namedtuple('Traverse_Action_i', 'ID Activity Domain Path Sou
 Action_i = namedtuple('Action_i', 'ID Activity Domain Wave')
 Flow_Dependency_i = namedtuple('Flow_Dependency_i', 'From_action To_action Activity Domain Flow Conditional Merge')
 Wave_i = namedtuple('Wave_i', 'Number Activity Domain')
+Wave_Assignment_i = namedtuple('Wave_Assignment_i', 'Action Activity Domain Wave')
 
 # Relationship subsystem
 Association_i = namedtuple('Association_i', 'Rnum Domain')
